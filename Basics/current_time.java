7784877237df86f8e854e4820cf276c85f485641
Gowthaman <gowthamanvai@gmail.com>
2020-02-05 16:22:14 -0800

    epoch time was included in currentDataand time -gowthaman

--- Basics/current_time.java
+++ Basics/current_time.java
@@ -1,6 +1,8 @@
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
+import java.time.Instant;
+
 
 public class CurrentDateAndTime
 {
@@ -12,15 +14,35 @@ public class CurrentDateAndTime
 
 		DateTimeFormatter formatter1=DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM);
 
-		DateTimeFormatter formatter3=DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss.SSS");
+		//DateTimeFormatter formatter3=DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss.SSS");
 
-		DateTimeFormatter formatter2=DateTimeFormatter.BASIC_ISO_DATE;
+		DateTimeFormatter formatter3=DateTimeFormatter.ofPattern("ddMMyyyyHHmmssSSS");
+
+		//DateTimeFormatter formatter2=DateTimeFormatter.BASIC_ISO_DATE;
+
+		//DateTimeFormatter formatter2=DateTimeFormatter.ISO_INSTENT;
+
+		DateTimeFormatter formatter2=DateTimeFormatter.ISO_LOCAL_DATE_TIME;
 
 		String formatted1=current.format(formatter1);
 
 		String formatted2=current.format(formatter2);
 
 		String formatted3=current.format(formatter3);
+
+		int epochsecond=(int)Instant.now().getEpochSecond();
+
+		long epoch=(long)Instant.now().toEpochMilli();
+
+		
+
+		String epochMilliSecond =Long.toString(epoch);
+
+		
+		System.out.println("The epoch time Second Second :" + epochsecond);
+
+		System.out.println("The epoch time milli Second Second :" + epochMilliSecond);
+
 
 		System.out.println("The Formatted1 current Date and Time is: " + formatted1);
 
